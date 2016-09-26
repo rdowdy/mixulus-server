c7e1dd0feaf7cba9bb8feb1a59ee67ca9675803a
Ryan Dowdy <rdowdy21@gmail.com>
2016-09-26 16:35:59 -0700

    Add Users schema. Add Users API endpoint.

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,20 @@
 'use strict'
 
 var express = require('express');
+var router = express.Router();
+var User = require("../models/user");
 
 var router = express.Router();
 
-//router.use(require("./users"));
+router.get("/users", function(req, res) {
+	User.find({}, function(err, users) {
+		if(err) {
+			// do something
+			res.status(500).json({message: err.message});
+		} else {
+			res.json({users: users});
+		}
+	})
+})
 
 module.exports = router;
--- src/api/users.js
+++
@@ -1,7 +0,0 @@
-'use strict'
- 
-var express = require("express");
-
-var users = null;
-
-module.exports = users;
