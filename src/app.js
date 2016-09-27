9a9ad7856f922c31154dea6abfc11b2c50c9ed15
Ryan Dowdy <rdowdy21@gmail.com>
2016-09-26 21:46:36 -0700

    Add some comments

--- src/app.js
+++ src/app.js
@@ -5,15 +5,19 @@ var clientPath = "/Users/rdowdy/dev/OCA/final_proj/client/app";
 var express = require('express');
 var router = require('./api');
 
+// initialize express
 var app = express();
 
+// initialize database and seed it
 require('./database');
 require('./seed');
 
+// set the root path
 app.use('/', express.static(clientPath));
-
+// set the api path
 app.use('/api', router);
 
+// start up the server
 app.listen(3000, function() {
 	console.log("The server is running on port 3000!");
 })
