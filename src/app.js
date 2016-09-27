5763087e4437ea739ce6d74eb616006f6f5f7d1d
Ryan Dowdy <rdowdy21@gmail.com>
2016-09-26 20:29:27 -0700

    Ignore DS_Store. Update path to client files. Remove a log from seed.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 'use strict';
 
+var clientPath = "../client";
+
 var express = require('express');
 var router = require('./api');
 
 var app = express();
 
 require('./database');
+require('./seed');
 
-app.use('/', express.static('public'));
+app.use('/', express.static(clientPath));
 
 app.use('/api', router);
 
