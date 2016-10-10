cb7290a756f868769411f54e1334c05e206bc982
Ryan Dowdy <rdowdy21@gmail.com>
2016-10-09 20:22:57 -0700

    Enable authentication. Comment out existing login strategy

--- src/passport/login.js
+++ src/passport/login.js
@@ -4,32 +4,34 @@ var bCrypt = require('bcrypt');
 
 module.exports = function(passport) {
 	console.log("Initializing login strategy...");
-	passport.use('login', new LocalStrategy(
-		{
-			passReqToCallback: true
-		},
-		function(req, email, password, done) {
-			console.log("Doing login");
-			// check to see if the email exists
-			// i'm using their email address as their sign-in
-			User.findOne({'email': email}, function(err, user) {
-				if(err) {
-					return done(err);
-				} else if(!user) {
-					console.log("User not found with email: " + email);
-					return done(null, false, req.flash("message", "Couldn't find user"))
-				}
+	// passport.use('login', new LocalStrategy(
+	// 	{
+	// 		passReqToCallback: true
+	// 	},
+	// 	function(req, email, password, done) {
+	// 		console.log("Doing login");
+	// 		// check to see if the email exists
+	// 		// i'm using their email address as their sign-in
+	// 		User.findOne({'email': email}, function(err, user) {
+	// 			if(err) {
+	// 				return done(err);
+	// 			} else if(!user) {
+	// 				console.log("User not found with email: " + email);
+	// 				return done(null, false, req.flash("message", "Couldn't find user"))
+	// 			}
 
-				if(!isValidPassword(user, password)) {
-					console.log('Invalid Password');
-					return done(null, false, req.flash('message', 'Invalid Password'));
-				}
+	// 			if(!isValidPassword(user, password)) {
+	// 				console.log('Invalid Password');
+	// 				return done(null, false, req.flash('message', 'Invalid Password'));
+	// 			}
 
-				// user and password both match, return
-				return done(null, user);
-			});
-		}
-	));
+	// 			// user and password both match, return
+	// 			return done(null, user);
+	// 		});
+	// 	}
+	// ));
+
+	
 
 	function isValidPassword(user, password) {
 		return bCrypt.compareSync(password, user.password);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var jwt = require('jwt-simple');
 var JwtStrategy = require('passport-jwt').Strategy;
 var router = express.Router();
 
@@ -7,7 +6,7 @@ var isAuthenticated = function (req, res, next) {
 	// if user is authenticated in the session, call the next() to call the next request handler 
 	// Passport adds this method to request object. A middleware is allowed to add properties to
 	// request and response objects
-	if (/*req.isAuthenticated()*/true)
+	if (req.isAuthenticated())
 		return next();
 	// if the user is not authenticated then redirect him to the login page
 	console.log(req);
@@ -29,11 +28,7 @@ module.exports = function(passport, path){
 		passport.authenticate('login', {
 			successRedirect: '/home',
 			failureRedirect: '/'
-		}),
-		function(req, res) {
-			var token = jwt.encode(req.user._id, 'superDuperSecret');
-			res.status(200).json({ state: 'success', token: token, user: req.user.username ? req.user : null})
-		});
+		}));
 
 	/* GET Registration Page */
 	router.get('/signup', function(req, res){
@@ -60,7 +55,6 @@ module.exports = function(passport, path){
 
 	/* Handle Logout */
 	router.get('/signout', function(req, res) {
-		console.log("Signing out");
 		req.logout();
 		res.redirect('/');
 	});
