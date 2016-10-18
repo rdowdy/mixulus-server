04d2b9ed7b3130a130a61dc506c067e1db8cd8fd
Ryan Dowdy <rdowdy21@gmail.com>
2016-10-18 11:26:03 -0700

    Use EC2 mongo URI

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 
-var db_url = "mongodb://localhost/collabio";
+var db_url = "mongodb://app:imGoingBack2505@ec2-54-226-99-158.compute-1.amazonaws.com:27017/collabio";
 
 mongoose.connect(db_url, function(err) {
 	if(err) {
