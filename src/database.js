89a1c789f8c65c3a7cc05d058059f5cc85c24445
Ryan Dowdy <rdowdy21@gmail.com>
2016-10-18 15:13:49 -0700

    Change URL of mongo

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 
-var db_url = "mongodb://app:imGoingBack2505@ec2-54-226-99-158.compute-1.amazonaws.com:27017/collabio";
+var db_url = "mongodb://app:imGoingBack2505@52.70.144.221:27017/collabio";
 
 mongoose.connect(db_url, function(err) {
 	if(err) {
