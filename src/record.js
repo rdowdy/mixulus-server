df815bd2589eb47cdbaea23bdc4861b2730da486
Ryan Dowdy <rdowdy21@gmail.com>
2016-10-23 14:49:30 -0700

    Clean up the socket server code. Scaffold the recording events

--- src/record.js
+++ src/record.js
@@ -7,66 +7,123 @@ var bodyParser = require('body-parser');
 var jwtVerifier = require('./passport/verify');
 var cors = require('cors');
 
+/////
+// environment configuration
+var production = process.env.PRODUCTION == 'true';
+var port = 9998;
+var ip = "127.0.0.1";
+
 var corsOptions = {
-    origin: "http://localhost:8080",
     credentials: true
 };
 
+if(production) {
+    corsOptions.origin = "https://mixulus.com";
+} else {
+    corsOptions.origin = "https://localhost:8080";
+}
+
+/////////////////////////////
+// Set up the server
+/////////////////////////////
 var socket_app = express();
+
+// middleware
 socket_app.use(require('morgan')('dev'));
 socket_app.use(cors(corsOptions));
 socket_app.use(bodyParser.urlencoded({ extended: false }));
 socket_app.use(express.static('static'));
 
-var port = 9998;
-var socket_server = socket_app.listen(port, "127.0.0.1", function() {
-    console.log("socket_app is running on port " + port);
+// start the server
+var socket_server = socket_app.listen(port, ip, function() {
+    console.log("The socket server is running on ip " + ip +  " at port " + port);
 });
 
-var wstream;
-var recBuffers;
-var tempBuffer;
-var recLen;
-var soundWritePath = "server/sounds/"
+/////////////////////////////
+// Set up the socket namespace
+/////////////////////////////
+var cfg = production ? { path: "/record" } : {};
+var io = require('socket.io')(socket_server, cfg);
 
-var io = require('socket.io')(socket_server/*, { path: "/record" }*/);
+var nsp;
+if(production) {
+    nsp = io.of('/record');
+} else {
+    nsp = io;
+}
 
-io/*.of('/record')*/.on('connection', function(socket) {
+/////////////////////////////
+// Set up the recording events
+/////////////////////////////
+var recSessions = {};
 
-    console.log("A user connected");
-    socket.on('start record', function(data) {
-        console.log("starting a recording session for " + data.id);
-        wstream = fs.createWriteStream(soundWritePath + data.id + '.pcm');
+nsp.on('connection', function(socket) {
+    // this event marks the beginning of a recording session
+    socket.on('start', function(data) {
 
-        recBuffers = [];
-        recLen = 0;
-
-        socket.emit("ready", {path: "/record"});
     });
 
-    socket.on('audio buffer', function(data) {
-        data.buffer.len = data.bufferLen;
-        recBuffers[data.bufferNum] = data.buffer;
-        recLen += data.bufferLen;
+    // push an audio buffer to a user's recording queue
+    socket.on('queue', function(data) {
+
     });
 
-    socket.on('done record', function(data) {
-        console.log("ending recording session for " + data.id);
-        // merge buffers 
-        var writeBuffer = mergeBuffers(recBuffers, recLen);
-        // now we need to turn the writeBuffer into
-        // an actual buffer
-        // 8 bytes per float
-        var actualBuffer = new Buffer(writeBuffer.length * 16);
-        for(var i = 0; i < writeBuffer.length; i++){
-            //write the float in Little-Endian and move the offset
-            actualBuffer.writeFloatBE(writeBuffer[i], i*16);
-        }
-        wstream.write(actualBuffer, function(err) { wstream.end(); });
-        // write to stream
-        //wstream.end();
+    // stop the recording session: 
+    //   - check for missing buffers
+    //   - merge and write the audio buffers
+    socket.on('stop', function(data) {
+
     });
 });
+
+// var wstream;
+// var recBuffers;
+// var tempBuffer;
+// var recLen;
+// var soundWritePath = "server/sounds/"
+
+// var io = require('socket.io')(socket_server/*, { path: "/record" }*/);
+
+// io/*.of('/record')*/.on('connection', function(socket) {
+
+//     console.log("A user connected");
+//     socket.on('start record', function(data) {
+//         console.log("starting a recording session for " + data.id);
+//         wstream = fs.createWriteStream(soundWritePath + data.id + '.pcm');
+
+//         recBuffers = [];
+//         recLen = 0;
+
+//         socket.emit("ready", {path: "/record"});
+//     });
+
+//     socket.on('audio buffer', function(data) {
+//         data.buffer.len = data.bufferLen;
+//         recBuffers[data.bufferNum] = data.buffer;
+//         recLen += data.bufferLen;
+//     });
+
+//     socket.on('done record', function(data) {
+//         console.log("ending recording session for " + data.id);
+//         // merge buffers 
+//         var writeBuffer = mergeBuffers(recBuffers, recLen);
+//         // now we need to turn the writeBuffer into
+//         // an actual buffer
+//         // 8 bytes per float
+//         var actualBuffer = new Buffer(writeBuffer.length * 16);
+//         for(var i = 0; i < writeBuffer.length; i++){
+//             //write the float in Little-Endian and move the offset
+//             actualBuffer.writeFloatBE(writeBuffer[i], i*16);
+//         }
+//         wstream.write(actualBuffer, function(err) { wstream.end(); });
+//         // write to stream
+//         //wstream.end();
+//     });
+// });
+
+/////////////////////////////
+// Helper Functions
+/////////////////////////////
 
 function mergeBuffers(buf, length) {
     var result = new Float32Array(length);
