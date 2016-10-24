c773efb0a76a4dd62df782e263de0db1ad090cdf
Ryan Dowdy <rdowdy21@gmail.com>
2016-10-23 20:57:27 -0700

    Clean up app server code

--- src/app.js
+++ src/app.js
@@ -1,8 +1,4 @@
 'use strict';
-
-var publicPath = process.env.PUBLIC_PATH;
-console.log("Root path is: " + publicPath);
-
 var fs = require('fs');
 var express = require('express');
 var passport = require('passport');
@@ -12,69 +8,76 @@ var bodyParser = require('body-parser');
 var jwtVerifier = require('./passport/verify');
 var cors = require('cors');
 
+////////////////////////
+// Environment Configuration
+////////////////////////
+var publicPath = process.env.PUBLIC_PATH;
+console.log("Root path is: " + publicPath);
+
 // initialize express
 var app = express();
 
-// initialize database and seed it
+// connect to database
 require('./database');
 
-// use body parser and cookie parser
-// and set the base path to serve static files from
+////////////////////////
+// Middleware
+////////////////////////
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ limit: '100mb' }));
 app.use(cookieParser());
 app.use(express.static(publicPath));
 
-/////////////////////////////
+////////////////////////
 // Passport
+////////////////////////
 
-// configuring passport
+// Configure 
 app.use(expressSession({
     secret: 'superDuperSecret',
     maxAge: 3600000
 }));
+
+// Initialize 
 app.use(passport.initialize());
 app.use(passport.session());
+require('./passport/init')(passport);
 
-// flash middleware
-// var flash = require('connect-flash');
-// app.use(flash());
-
-// Initialize Passport
-var initPassport = require('./passport/init');
-initPassport(passport);
-
-/////////////////////////////
-
-/////////////////////////////
-// Set up routes
+////////////////////////
+// API Routes
+////////////////////////
 
 var routes = require('./routes/index')(passport, publicPath);
 
-/* Use JWT verify middlware to authenticate API endpoint routes */
+// Use JWT verification to check API routes authorization
 routes.use(jwtVerifier);
 
+// Require the routing code
 var collabRoute = require("./routes/collab.route.js");
-routes.use("/collabs", collabRoute);
-
 var trackRoute = require("./routes/track.route.js");
-routes.use("/tracks", trackRoute);
-
 var soundRoute = require("./routes/sound.route.js");
-routes.use("/sounds", soundRoute);
-
 var userRoute = require("./routes/user.route.js");
+
+// Use the routing code on these routes
+routes.use("/collabs", collabRoute);
+routes.use("/tracks", trackRoute);
+routes.use("/sounds", soundRoute);
 routes.use("/users", userRoute);
 
+// Use API routes on app server
 app.use(routes);
 
-// start up the server
+////////////////////////
+// Start the App Server
+////////////////////////
 var port = process.env.PORT || 8080;
 app.listen(port, "127.0.0.1", function() {
     console.log("The server is running on port " + port);
 })
 
-// the socket server takes care of a user's recording session
-var socketServer = require("./record.js");
+////////////////////////
+// Socket server used for recording
+////////////////////////
+require("./record.js");
 
 module.exports = app;
--- src/passport/init.js
+++ src/passport/init.js
@@ -8,37 +8,14 @@ module.exports = function(passport) {
     console.log("Initializing passport...");
     // Passport needs to be able to serialize and deserialize users to support persistent login sessions
     passport.serializeUser(function(user, done) {
-        //console.log('serializing user: ');
-        //console.log(user);
         done(null, user._id);
     });
 
     passport.deserializeUser(function(id, done) {
         User.findById(id, function(err, user) {
-            //console.log('deserializing user:', user);
             done(err, user);
         });
     });
-
-    var opts = {}
-    opts.jwtFromRequest = ExtractJwt.fromBodyField();
-    opts.secretOrKey = 'superDuperSecret';
-    passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-        console.log('authenticating');
-        User.findOne({ _id: jwt_payload.sub }, function(err, user) {
-            if (err) {
-                return done(err, false);
-            }
-            if (user) {
-                // account found, now verify password
-
-                return done(null, user);
-            } else {
-                // no account found
-                return done(null, false);
-            }
-        });
-    }));
 
     // Setting up Passport Strategies for Login and SignUp/Registration
     login(passport);
